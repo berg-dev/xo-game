c24452584a90736f3bddd2c58b85860b8a39b375
Andrey Berg <berg@MacBook-Pro.local>
2018-10-11 18:53:23 +0500

    implement basic game logic

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,8 @@
 export default class Game {
   constructor() {
-    this.state = [
+    this._userMoveSymbol = '×';
+    this._computerMoveSymbol = 'o';
+    this._board = [
       ['', '', ''],
       ['', '', ''],
       ['', '', ''],
@@ -8,6 +10,33 @@ export default class Game {
   }
 
   getState() {
-    return this.state;
+    return this._board;
+  }
+
+  acceptUserMove(x, y) {
+    if (!this._isCellFree(x, y)) {
+      return this._throwException('ячейка уже занята');
+    }
+
+    return this._updateBoard(x, y);
+  }
+
+  createComputerMove() {
+    this._updateBoard(0, 0, {
+      symbol: this._computerMoveSymbol,
+    });
+  }
+
+  _updateBoard(x, y, config = {}) {
+    const { symbol = this._userMoveSymbol } = config;
+    this._board[x][y] = symbol;
+  }
+
+  _isCellFree(x, y) {
+    return !this._board[x][y];
+  }
+
+  _throwException(msg) {
+    throw new Error(msg);
   }
 }
--- test/game.js
+++ test/game.js
@@ -2,6 +2,7 @@ import { expect } from 'chai';
 import { describe, it, beforeEach } from 'mocha';
 import Game from '../src/Game';
 
+const userMoveSymbol = '×';
 const initialGameBoard = [
   ['', '', ''],
   ['', '', ''],
@@ -16,5 +17,32 @@ describe('Game', () => {
     const board = game.getState();
 
     expect(board).to.deep.equal(initialGameBoard);
+  });
+
+  it('Записывает символ пользователя в ячейку по переданным координатам', () => {
+    const x = 1;
+    const y = 1;
+
+    game.acceptUserMove(x, y);
+    const board = game.getState();
+
+    expect(board[x][y]).to.equal(userMoveSymbol);
+  });
+
+  it('Выкидывает исключение если пользователь ходит в занятую ячейку', () => {
+    const x = 2;
+    const y = 2;
+
+    game.acceptUserMove(x, y);
+    const func = game.acceptUserMove.bind(game, x, y);
+
+    expect(func).to.throw('ячейка уже занята');
+  });
+
+  it('Компьютер ходит в верхнюю левую ячейку', () => {
+    game.createComputerMove();
+    const board = game.getState();
+
+    expect(board[0][0]).to.equal('o');
   });
 });
